0e6dcbfe708bcb2459a080a8baae1031eb90eff4
Michael Yao <sbubby@Michaels-MacBook-Pro.local>
2022-12-10 15:21:55 -0600

    Add method headers for mfs.c

--- mfs.c
+++ mfs.c
@@ -19,7 +19,65 @@ int MFS_Init(char *hostname, int port)
     return 0;
 }
 
-// returns some information about the file specified by inum. Upon success, return 0, otherwise -1. The exact info returned is defined by MFS_Stat_t. Failure modes: inum does not exist. File and directory sizes are described below.
+/*
+ * returns some information about the file specified by inum.
+ * Upon success, return 0, otherwise -1.
+ * The exact info returned is defined by MFS_Stat_t.
+ * Failure modes: inum does not exist.
+ * File and directory sizes are described below.
+ */
 int MFS_Stat(int inum, MFS_Stat_t *m)
 {
+    return -1;  
+}
+
+/*
+ * writes a buffer of size nbytes (max size: 4096 bytes) at the byte offset specified by offset.
+ * Returns 0 on success, -1 on failure.
+ * Failure modes: invalid inum, invalid nbytes, invalid offset, not a regular file (because you can't write to directories).
+*/
+int MFS_Write(int inum, char *buffer, int offset, int nbytes)
+{
+    return -1;
+}
+
+/**
+ * reads nbytes of data (max size 4096 bytes) specified by the byte offset offset into the buffer from file specified by inum.
+ * The routine should work for either a file or directory; directories should return data in the format specified by MFS_DirEnt_t.
+ * Success: 0, failure: -1. Failure modes: invalid inum, invalid offset, invalid nbytes.
+*/
+int MFS_Read(int inum, char *buffer, int offset, int nbytes)
+{
+    return -1;
+}
+
+/**
+ * makes a file (type == MFS_REGULAR_FILE) or directory (type == MFS_DIRECTORY)
+ * in the parent directory specified by pinum of name name.
+ * Returns 0 on success, -1 on failure.
+ * Failure modes: pinum does not exist, or name is too long. If name already exists, return success.
+*/
+int MFS_Creat(int pinum, int type, char *name)
+{
+    return -1;
+}
+
+/**
+ * removes the file or directory name from the directory specified by pinum.
+ * 0 on success, -1 on failure.
+ * Failure modes: pinum does not exist, directory is NOT empty.
+ * Note that the name not existing is NOT a failure by our definition (think about why this might be).
+*/
+int MFS_Unlink(int pinum, char *name)
+{
+    return -1;
+}
+
+/**
+ * ust tells the server to force all of its data structures to disk and shutdown by calling exit(0).
+ * This interface will mostly be used for testing purposes.
+*/
+int MFS_Shutdown()
+{
+    return -1;
 }
