c8899c80107bb0ac1d59bf4e6fc962f6b8dd518b
ngimelliUW <gimelli@wisc.edu>
2022-12-10 15:13:36 -0600

    add method descriptions for stat and init

--- mfs.c
+++ mfs.c
@@ -4,6 +4,7 @@
 int fd;
 struct sockaddr_in server_addr;
 
+//  takes a host name and port number and uses those to find the server exporting the file system
 int MFS_Init(char *hostname, int port)
 {
     int rc = UDP_FillSockAddr(&server_addr, hostname, port);
@@ -16,4 +17,9 @@ int MFS_Init(char *hostname, int port)
 
     fd = UDP_Open(port);
     return 0;
+}
+
+// returns some information about the file specified by inum. Upon success, return 0, otherwise -1. The exact info returned is defined by MFS_Stat_t. Failure modes: inum does not exist. File and directory sizes are described below.
+int MFS_Stat(int inum, MFS_Stat_t *m)
+{
 }
