8f20d306df4ee3e71f48a5e0226880c0d78aff1b
Nicolas Gimelli <gimelli@vm-instunix-05.cs.wisc.edu>
2022-12-12 19:35:29 -0600

    add udp_close in mfs_shutdown

--- mfs.c
+++ mfs.c
@@ -110,6 +110,7 @@ int MFS_Shutdown()
     UDP_Write(fd, &server_addr, (char *)&msg, sizeof(msg_t));
 
     UDP_Read(fd, &server_addr, (char *)&msg, sizeof(msg_t));
-    exit(0);
-    return -1;
+
+    UDP_Close(fd);
+    return 0;
 }
